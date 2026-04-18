e3a8d6d5707e56f407f5080d21a7099b6c8c72c9
varunraj30 <rajvarun0402@gmail.com>
2026-04-18 14:35:13 +0530

    hello world

---
+++ helloworld/index.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+
+console.log(window);
+
+console.log(alert);
+
+// From V8 Engine, DOM related elements have been removed (other elements like crypto and file handling have been added) and embedded with c
