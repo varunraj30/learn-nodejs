59678d8e6e0bf9392390e1a1714109bbed2e2086
varunraj30 <rajvarun0402@gmail.com>
2026-04-19 11:59:07 +0530

    learn status code

--- proj1-CRUD-express/index.js
+++ proj1-CRUD-express/index.js
@@ -61,7 +61,7 @@ app.post("/api/users", (req, res) => {
   const body = req.body;
   users.push({ ...body, id: users.length + 1 });
   fs.writeFile("./data.json", JSON.stringify(users), (err, data) => {
-    return res.json({ status: "success", id: users.length });
+    return res.status(201).json({ status: "success", id: users.length });
   });
 });
 
