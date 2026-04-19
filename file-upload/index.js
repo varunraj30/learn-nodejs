aa7c228c7b1a436a2aabef5112045140acbd4502
varunraj30 <rajvarun0402@gmail.com>
2026-04-20 00:48:04 +0530

    learn multer

---
+++ file-upload/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const path = require("path");
+const multer = require("multer");
+
+const app = express();
+const PORT = 8000;
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    return cb(null, "./uploads");
+  },
+  filename: function (req, file, vb) {
+    return cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  return res.render("homepage");
+});
+
+app.post(
+  "/upload",
+  upload.array([{ name: "profileImage" }, { name: "coverImage" }]),
+  (req, res) => {
+    console.log(req.body); // undefined
+    console.log(req.file);
+
+    return res.redirect("/");
+  },
+);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT ${PORT}`);
+});
